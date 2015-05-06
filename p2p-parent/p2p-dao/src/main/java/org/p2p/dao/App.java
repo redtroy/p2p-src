d18d1288839001eaa0f4d1d9c15ce8d3bbaa8cc2
redtroy <redtroy@sxj.com>
2015-05-06 10:42:03 +0800

    Change-Id: I238d43e899ad6714286393f8e12fc17540b9999d

---
+++ p2p-parent/p2p-dao/src/main/java/org/p2p/dao/App.java
@@ -0,0 +1,13 @@
+package org.p2p.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
