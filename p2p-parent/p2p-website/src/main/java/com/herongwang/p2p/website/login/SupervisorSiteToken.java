2d575148678c405927b8ea8cede48b9007ae78c6
qjj[merge] <qjj@sxj.com>
2015-05-08 10:16:32 +0800

    qjj

---
+++ p2p-parent/p2p-website/src/main/java/com/herongwang/p2p/website/login/SupervisorSiteToken.java
@@ -0,0 +1,13 @@
+package com.herongwang.p2p.website.login;
+
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+public class SupervisorSiteToken extends UsernamePasswordToken
+{
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7748781410357940194L;
+    
+}
