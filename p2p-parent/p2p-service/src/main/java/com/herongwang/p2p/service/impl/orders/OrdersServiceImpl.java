da6a7091e5105d6743309e13cceb1b6a8586e531
nist <nishaotang@126.com>
2015-05-11 17:47:14 +0800

    Signed-off-by: nist <nishaotang@126.com>

---
+++ p2p-parent/p2p-service/src/main/java/com/herongwang/p2p/service/impl/orders/OrdersServiceImpl.java
@@ -0,0 +1,57 @@
+package com.herongwang.p2p.service.impl.orders;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.herongwang.p2p.dao.orders.IOrdersDao;
+import com.herongwang.p2p.entity.orders.OrdersEntity;
+import com.herongwang.p2p.model.member.MemberModel;
+import com.herongwang.p2p.service.orders.IOrdersService;
+import com.sxj.util.exception.ServiceException;
+import com.sxj.util.persistent.QueryCondition;
+@Service
+public class OrdersServiceImpl implements IOrdersService
+{
+    @Autowired
+    private IOrdersDao ordersDao;
+    
+    @Override
+    public void addOrders(OrdersEntity orders) throws ServiceException
+    {
+        ordersDao.addOrders(orders);
+        
+    }
+    
+    @Override
+    public void updateOrders(OrdersEntity orders) throws ServiceException
+    {
+        ordersDao.updateOrders(orders);
+        
+    }
+    
+    @Override
+    public OrdersEntity getOrdersEntity(String id) throws ServiceException
+    {
+        return ordersDao.getOrders(id);
+    }
+    
+    @Override
+    public List<OrdersEntity> queryOrdersList(OrdersEntity query)
+            throws ServiceException
+    {
+        QueryCondition<OrdersEntity> condition = new QueryCondition<OrdersEntity>();
+        condition.addCondition("customerId", query.getCustomerId());
+        List<OrdersEntity> ordersList = ordersDao.query(condition);
+        return ordersList;
+    }
+    
+    @Override
+    public void delOrders(String id) throws ServiceException
+    {
+        ordersDao.delOrders(id);
+        
+    }
+    
+}
