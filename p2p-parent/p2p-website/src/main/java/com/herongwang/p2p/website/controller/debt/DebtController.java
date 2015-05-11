490cfc6763dd03c357ee00d2a5de773abbd518e6
anshaoshuai <anshaoshuai@scj.com>
2015-05-11 14:48:06 +0800

    2015/5/11

---
+++ p2p-parent/p2p-website/src/main/java/com/herongwang/p2p/website/controller/debt/DebtController.java
@@ -0,0 +1,40 @@
+package com.herongwang.p2p.website.controller.debt;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.herongwang.p2p.entity.debt.DebtEntity;
+import com.herongwang.p2p.service.tender.IDebtService;
+
+@Controller
+@RequestMapping("/debt")
+public class DebtController
+{
+    @Autowired
+    IDebtService debtService;
+    
+    /**
+     * 跳转申请融资
+     * @param session
+     * @param map
+     * @return
+     */
+    @RequestMapping("queryDebt")
+    public String queryDebt(HttpSession session, ModelMap map, DebtEntity query)
+    {
+        
+        //获取会员信息
+        query.setCustomerId("1");
+        List<DebtEntity> debtList = debtService.queryDebtList(query);
+        map.put("debtList", debtList);
+        map.put("query", query);
+        
+        return "site/debt/debtList";
+    }
+}
