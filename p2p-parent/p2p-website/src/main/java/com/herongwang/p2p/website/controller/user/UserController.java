11ff0257a1a20e69f6f0b0e2b56800430538a485
qjj[merge] <qjj@sxj.com>
2015-05-11 20:16:46 +0800

    qjj

---
+++ p2p-parent/p2p-website/src/main/java/com/herongwang/p2p/website/controller/user/UserController.java
@@ -0,0 +1,79 @@
+package com.herongwang.p2p.website.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.herongwang.p2p.entity.users.UsersEntity;
+import com.herongwang.p2p.service.account.IAccountService;
+import com.herongwang.p2p.service.users.IUserService;
+import com.sxj.util.exception.WebException;
+import com.sxj.util.logger.SxjLogger;
+
+@Controller
+@RequestMapping("user")
+public class UserController
+{
+    @Autowired
+    private IUserService userService;
+    
+    @Autowired
+    private IAccountService accountService;
+    
+    @RequestMapping("register")
+    public String register(ModelMap map, UsersEntity member)
+            throws WebException
+    {
+        return "site/member/register";
+    }
+    
+    @RequestMapping("memberInfo")
+    public String memberInfo(ModelMap map) throws WebException
+    {
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error(e.getMessage(), e, this.getClass());
+            throw new WebException("查询用户信息出错", e);
+        }
+        return "site/member/member-center";
+    }
+    
+    @RequestMapping("saveUser")
+    public String saveMember(UsersEntity user, ModelMap map)
+            throws WebException
+    {
+        try
+        {
+            UsersEntity info = userService.addUser(user);
+            map.put("user", info);
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error(e.getMessage(), e, this.getClass());
+            throw new WebException("保存用户信息出错", e);
+        }
+        return "site/member/member-center";
+    }
+    
+    @RequestMapping("updateUser")
+    public String updateUser(ModelMap map, UsersEntity user)
+            throws WebException
+    {
+        try
+        {
+            UsersEntity info = userService.updateUser(user);
+            map.put("user", info);
+        }
+        catch (Exception e)
+        {
+            SxjLogger.error(e.getMessage(), e, this.getClass());
+            throw new WebException("保存用户信息出错", e);
+        }
+        return "site/member/member-center";
+    }
+}
