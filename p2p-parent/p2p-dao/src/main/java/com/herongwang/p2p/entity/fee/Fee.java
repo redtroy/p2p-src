70d1546cd555227b1f50c86c73cb74ab72a91ff4
redtroy <redtroy@sxj.com>
2015-05-11 14:45:05 +0800

    Change-Id: Ib04b9d8178cc9ff1cfba3ec67c14eb5c777692ed

---
+++ p2p-parent/p2p-dao/src/main/java/com/herongwang/p2p/entity/fee/Fee.java
@@ -0,0 +1,134 @@
+/***********************************************************************
+ * Module:  Fee.java
+ * Author:  Administrator
+ * Purpose: Defines the Class Fee
+ ***********************************************************************/
+package com.herongwang.p2p.entity.fee;
+
+import java.util.Date;
+
+import com.sxj.mybatis.orm.annotations.Column;
+import com.sxj.mybatis.orm.annotations.Entity;
+import com.sxj.mybatis.orm.annotations.GeneratedValue;
+import com.sxj.mybatis.orm.annotations.GenerationType;
+import com.sxj.mybatis.orm.annotations.Id;
+import com.sxj.mybatis.orm.annotations.Table;
+
+/** 费率
+ * 
+ * @pdOid 143487cb-be68-4c13-bde7-77e5c09a8b01 */
+@Entity
+@Table(name = "Fee")
+public class Fee
+{
+    /** 费率ID
+     * 
+     * @pdOid 30d7f7c6-e800-434a-9d28-f335ed8e0101 */
+    @Id(column = "feeId")
+    @GeneratedValue(strategy = GenerationType.UUID, length = 32)
+    private java.lang.String feeId;
+    
+    /** 费率
+     * 
+     * @pdOid 7fdde590-efd1-4248-b49c-d594a16471f4 */
+    @Column(name = "fee")
+    private java.lang.Float fee;
+    
+    /** 级别
+     * 
+     * @pdOid c276c304-6020-40ae-887a-0705b27a736f */
+    @Column(name = "level")
+    private int level;
+    
+    /** 状态
+     * 
+     * @pdOid 36eaa55f-354d-4dbe-9404-b4676f856dc8 */
+    @Column(name = "status")
+    private FeeStatus status = FeeStatus.AVAILABLE;
+    
+    /** 创建时间
+     * 
+     * @pdOid 635da763-14c6-4d7a-aaad-df4e679b3a45 */
+    @Column(name = "createTime")
+    private java.util.Date createTime = new Date();
+    
+    /** 更新时间
+     * 
+     * @pdOid c4ded15d-98d1-47c4-ae9e-b20a4baac5df */
+    @Column(name = "updateTime")
+    private java.util.Date updateTime;
+    
+    @Column(name = "parentId")
+    private String parentId;
+    
+    public java.lang.String getFeeId()
+    {
+        return feeId;
+    }
+    
+    public void setFeeId(java.lang.String feeId)
+    {
+        this.feeId = feeId;
+    }
+    
+    public java.lang.Float getFee()
+    {
+        return fee;
+    }
+    
+    public void setFee(java.lang.Float fee)
+    {
+        this.fee = fee;
+    }
+    
+    public int getLevel()
+    {
+        return level;
+    }
+    
+    public void setLevel(int level)
+    {
+        this.level = level;
+    }
+    
+    public java.util.Date getCreateTime()
+    {
+        return createTime;
+    }
+    
+    public void setCreateTime(java.util.Date createTime)
+    {
+        this.createTime = createTime;
+    }
+    
+    public java.util.Date getUpdateTime()
+    {
+        return updateTime;
+    }
+    
+    public void setUpdateTime(java.util.Date updateTime)
+    {
+        this.updateTime = updateTime;
+    }
+    
+    public String getParentId()
+    {
+        return parentId;
+    }
+    
+    public void setParentId(String parentId)
+    {
+        this.parentId = parentId;
+    }
+    
+    public FeeStatus getStatus()
+    {
+        return status;
+    }
+    
+    public void setStatus(FeeStatus status)
+    {
+        this.status = status;
+    }
+    
+}
---
+++ p2p-parent/p2p-dao/src/main/java/com/herongwang/p2p/entity/member/UserLevel.java
@@ -0,0 +1,103 @@
+/***********************************************************************
+ * Module:  UserLevel.java
+ * Author:  Administrator
+ * Purpose: Defines the Class UserLevel
+ ***********************************************************************/
+package com.herongwang.p2p.entity.member;
+
+import com.sxj.mybatis.orm.annotations.Column;
+import com.sxj.mybatis.orm.annotations.Entity;
+import com.sxj.mybatis.orm.annotations.GeneratedValue;
+import com.sxj.mybatis.orm.annotations.GenerationType;
+import com.sxj.mybatis.orm.annotations.Id;
+import com.sxj.mybatis.orm.annotations.Table;
+
+/** 用户级别
+ * 
+ * @pdOid 51ae8418-ec05-404f-b828-0e15a20904c9 */
+@Entity
+@Table(name = "UserLevel")
+public class UserLevel
+{
+    /** 级别ID
+     * 
+     * @pdOid 0bbcf285-e9e2-4c45-91fd-b3a27a5add2e */
+    @Id(column = "levelId")
+    @GeneratedValue(strategy = GenerationType.UUID, length = 32)
+    private java.lang.String levelId;
+    
+    /** 级别名称
+     * 
+     * @pdOid 53711d67-fd77-47e8-a6ba-cef6f44f6419 */
+    @Column(name = "name")
+    private java.lang.String name;
+    
+    /** 级别状态
+     * 
+     * @pdOid 5a2d9230-ac21-4068-ab7c-8f0445b62d29 */
+    @Column(name = "status")
+    private int status;
+    
+    /** 创建时间
+     * 
+     * @pdOid 446b2d02-6554-4f85-9756-9f5d166d1977 */
+    @Column(name = "createTime")
+    private java.util.Date createTime;
+    
+    /** 更新时间
+     * 
+     * @pdOid 703f3b74-1d6e-4601-991c-c3ab17f84166 */
+    @Column(name = "updateTime")
+    private java.util.Date updateTime;
+    
+    public java.lang.String getLevelId()
+    {
+        return levelId;
+    }
+    
+    public void setLevelId(java.lang.String levelId)
+    {
+        this.levelId = levelId;
+    }
+    
+    public java.lang.String getName()
+    {
+        return name;
+    }
+    
+    public void setName(java.lang.String name)
+    {
+        this.name = name;
+    }
+    
+    public int getStatus()
+    {
+        return status;
+    }
+    
+    public void setStatus(int status)
+    {
+        this.status = status;
+    }
+    
+    public java.util.Date getCreateTime()
+    {
+        return createTime;
+    }
+    
+    public void setCreateTime(java.util.Date createTime)
+    {
+        this.createTime = createTime;
+    }
+    
+    public java.util.Date getUpdateTime()
+    {
+        return updateTime;
+    }
+    
+    public void setUpdateTime(java.util.Date updateTime)
+    {
+        this.updateTime = updateTime;
+    }
+    
+}
