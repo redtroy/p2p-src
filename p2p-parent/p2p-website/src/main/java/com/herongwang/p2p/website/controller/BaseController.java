a7f57b58377c8dfec631a7808b4ff32f01c5fe85
qjj[merge] <qjj@sxj.com>
2015-05-11 15:12:06 +0800

    qjj

--- p2p-parent/p2p-website/src/main/java/com/herongwang/p2p/website/controller/BaseController.java
+++ p2p-parent/p2p-website/src/main/java/com/herongwang/p2p/website/controller/BaseController.java
@@ -20,7 +20,7 @@ public class BaseController
     
     public static final String LOGIN = "site/member/login";
     
-    // public static final String INDEX = "site/index";
+    public static final String INDEX = "site/index";
     
     public static final String SPRING_SECURITY_CONTEXT = "SPRING_SECURITY_CONTEXT";
     
