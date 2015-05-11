c5226354c71fc2cbbfa0852bdb6dc4fa658e0e06
redtroy <redtroy@sxj.com>
2015-05-11 16:21:11 +0800

    Change-Id: Iaf2249befe4e472438376d0af427d26a7bf847fb

---
+++ p2p-parent/p2p-dao/src/test/java/com/herongwang/p2p/dao/TestBase.java
@@ -0,0 +1,14 @@
+package com.herongwang.p2p.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml" })
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+public class TestBase
+{
+    
+}
