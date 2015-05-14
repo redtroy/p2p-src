f6404ad7945739df3e2df9f95d5a5a5327c0adbe
redtroy <redtroy@sxj.com>
2015-05-14 09:58:00 +0800

    Change-Id: I81e7a5b2b36cc51dcce716a76c9f5a90f687d416

---
+++ p2p-parent/allinpay-signtool/src/main/java/org/allinpay/signtool/App.java
@@ -0,0 +1,13 @@
+package org.allinpay.signtool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
